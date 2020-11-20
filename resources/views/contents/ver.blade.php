670230bc9202af9115e0e38549a18cd78071d698
Jesus Patlan <jpatlancastillo@gmail.com>
2020-11-20 13:42:02 -0600

    a

--- resources/views/contents/ver.blade.php
+++ resources/views/contents/ver.blade.php
@@ -6,8 +6,8 @@
 @section('content')
 
     <img src="data:image/png;base64,{{ $baseData }}" alt="Red dot" />
-    <video src="data:video/mp4;base64,{{ $baseData }}" alt="Red dot" />
     <iframe src="data:application/pdf;base64,{{ $baseData }}" height="100%" width="100%"></iframe>
+    <video src="data:video/mp4;base64,{{ $baseData }}" alt="Red dot" />
 
 
 @stop
